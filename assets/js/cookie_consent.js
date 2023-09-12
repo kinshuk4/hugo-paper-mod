4c4b0fe43be995a5f8b6e9673fac87ca0dbb4bca
Kinshuk <kinshuk.ram@gmail.com>
2023-09-12 22:49:27 +0200

    Finally the consent works.

--- assets/js/cookie_consent.js
+++ assets/js/cookie_consent.js
@@ -1,16 +1,20 @@
 // Cookie consent logic
-const cookieConsent = document.getElementById('cookie-consent');
-const acceptCookiesButton = document.getElementById('accept-cookies');
 
-// Check if the user has previously accepted cookies
-const hasConsented = localStorage.getItem('cookie-consent-accepted');
-
-if (!hasConsented) {
-  cookieConsent.style.display = 'block';
+function hideDisclaimer() {
+  document.getElementById('disclaimer-banner').style.display = 'none';
 }
 
-acceptCookiesButton.addEventListener('click', () => {
-  // Set a flag in local storage to remember user consent
-  localStorage.setItem('cookie-consent-accepted', 'true');
-  cookieConsent.style.display = 'none';
-});
+(function() {
+  if (localStorage.getItem('disclaimer-confirm')) {
+    hideDisclaimer();
+  }
+})();
+
+// document.getElementById("b-btn").addEventListener('click', function() {
+//   localStorage.setItem('disclaimer-confirm', 'true');
+//   hideDisclaimer();
+// })
+function handleBtnClick() {
+  localStorage.setItem('disclaimer-confirm', 'true');
+  hideDisclaimer();
+}
